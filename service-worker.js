7297058d9fddf44e447e073932e523efec9c2cf7
Alex <alexmogwaiii@gmail.com>
2021-01-21 16:07:01 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.8722b622c1afe447c71253c545cedc2b.js"
 );
 
 self.addEventListener('message', (event) => {
